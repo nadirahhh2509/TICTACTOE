7fdc635141101c8c6b2a1a3785cd1b3f57129b3f
nadirahhh2509 <B022210189@student.utem.edu.my>
2025-01-20 18:01:43 +0800

    Add token and role fields to User model; update export statement

--- src/models/User.js
+++ src/models/User.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    token: { type: String, required: true },
+    role: { type: String, enum: ['user', 'admin'], default: 'user' }
 });
 
-module.exports = mongoose.model('User', UserSchema);
+//const Collection = new mongoose.model("AuthCollection", UserSchema);
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
